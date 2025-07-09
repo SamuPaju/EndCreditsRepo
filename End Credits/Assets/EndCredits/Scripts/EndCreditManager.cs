9de0eb3beba9f284d0c85a27d73ddf3ab895222d
SamuPaju <s2300894@edu.bc.fi>
2025-07-09 13:42:04 +0300

    Update
    Made the different text types and some needed variables.

--- End Credits/Assets/EndCredits/Scripts/EndCreditManager.cs
+++ End Credits/Assets/EndCredits/Scripts/EndCreditManager.cs
@@ -7,8 +7,19 @@ public class EndCreditManager : MonoBehaviour
 {
     public static EndCreditManager instance;
 
+    [Header("Text modifiers")]
     public int rollSpeed = 5;
     public TMP_FontAsset font;
+
+    [Header("Text templates")]
+    [SerializeField] GameObject headlineBase;
+    [SerializeField] GameObject groupLineBase;
+    [SerializeField] GameObject nameLine;
+
+    [Header("Credit texts")]
+    [SerializeField] List<string> creditSequence;
+
+    [SerializeField] GameObject startPoint;
 
     private void Awake()
     {
