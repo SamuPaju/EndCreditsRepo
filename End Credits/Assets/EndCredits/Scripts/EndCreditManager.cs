1a81d72bd5bb408467940c219f706072b47422cb
SamuPaju <s2300894@edu.bc.fi>
2025-07-10 19:00:56 +0300

    Update (Broken)
    Continued making the main script but I have to go now.
    The script now prints the different lines but without any break and multiple times. there are some special keys to determine what type of text is created.

--- End Credits/Assets/EndCredits/Scripts/EndCreditManager.cs
+++ End Credits/Assets/EndCredits/Scripts/EndCreditManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+/// <summary>
+/// Manage end credits
+/// </summary>
 public class EndCreditManager : MonoBehaviour
 {
     public static EndCreditManager instance;
@@ -14,12 +17,19 @@ public class EndCreditManager : MonoBehaviour
     [Header("Text templates")]
     [SerializeField] GameObject headlineBase;
     [SerializeField] GameObject groupLineBase;
-    [SerializeField] GameObject nameLine;
+    [SerializeField] GameObject nameLineBase;
 
     [Header("Credit texts")]
     [SerializeField] List<string> creditSequence;
 
-    [SerializeField] GameObject startPoint;
+    // Special keys
+    string[] specialKeysHolder = new string[] { "/h", "/g" };
+    //string headlineKey = "/h";
+    //string groupLine = "/g";
+
+    [SerializeField] Transform startPoint;
+    [SerializeField] Transform endPoint;
+    [SerializeField] Transform textParent;
 
     private void Awake()
     {
@@ -30,8 +40,57 @@ public class EndCreditManager : MonoBehaviour
         else { instance = this; }
     }
 
-    private void Update()
+    private void Start()
     {
-        
+        for (int i = 0; i < creditSequence.Count; i++)
+        {
+            creditSequence[i].Trim();
+
+            foreach (string key in specialKeysHolder)
+            {
+                if (creditSequence[i].Contains(key))
+                {
+                    // /h scenario
+                    if (key == creditSequence[1].ToString())
+                    {
+                        TextMeshProUGUI text = headlineBase.GetComponent<TextMeshProUGUI>();
+                        text.text = creditSequence[i].ToString();
+                        CreateLine(headlineBase);
+                        StartCoroutine(waitTime(5));
+                    }
+                    // /g scenario
+                    else
+                    {
+                        TextMeshProUGUI text = groupLineBase.GetComponent<TextMeshProUGUI>();
+                        text.text = creditSequence[i].ToString();
+                        CreateLine(groupLineBase);
+                        StartCoroutine(waitTime(3));
+                    }
+                }
+                // Default scenario
+                else
+                {
+                    TextMeshProUGUI text = nameLineBase.GetComponent<TextMeshProUGUI>();
+                    text.text = creditSequence[i].ToString();
+                    CreateLine(nameLineBase);
+                    StartCoroutine(waitTime(2));
+                }
+            }
+        }
+        StartCoroutine(waitTime(2));
+    }
+
+    /// <summary>
+    /// Instantiate the text object
+    /// </summary>
+    /// <param name="textObject"></param>
+    void CreateLine(GameObject textObject)
+    {
+        Instantiate(textObject, startPoint.position, textObject.transform.rotation, textParent);
+    }
+
+    IEnumerator waitTime(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
     }
 }
